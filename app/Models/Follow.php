625831ceebf17f6879b28079d997f8cd98e3b3b1
Akamya <elodie.langlet@gmail.com>
2024-10-21 22:12:25 +0200

    improve likes & follows models

--- app/Models/Follow.php
+++ app/Models/Follow.php
@@ -9,4 +9,16 @@ class Follow extends Model
 {
     /** @use HasFactory<\Database\Factories\FollowFactory> */
     use HasFactory;
+
+    // Utilisateur qui suit
+    public function follower()
+    {
+        return $this->belongsTo(User::class, 'follower_id');
+    }
+
+    // Utilisateur suivi
+    public function followed()
+    {
+        return $this->belongsTo(User::class, 'followed_id');
+    }
 }
