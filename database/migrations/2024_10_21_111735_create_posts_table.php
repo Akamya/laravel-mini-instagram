dca6be4bb03b8279bc4f8bd119c51933b205415a
Akamya <elodie.langlet@gmail.com>
2024-10-21 13:31:59 +0200

    post table

--- database/migrations/2024_10_21_111735_create_posts_table.php
+++ database/migrations/2024_10_21_111735_create_posts_table.php
@@ -13,6 +13,10 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->text('body')->nullable();
+            $table->string('img_path');
+            $table->timestampTz('published_at')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->timestamps();
         });
     }
