0d379caa37c6ba21da51d213aa8b3e23d4e147c3
Akamya <elodie.langlet@gmail.com>
2024-10-27 16:24:35 +0100

    recherche utilisateurs ou posts dans feed

--- app/Http/Controllers/FeedController.php
+++ app/Http/Controllers/FeedController.php
@@ -3,19 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Post;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class FeedController extends Controller
 {
-    // Affiche les posts sur la page feed avec un paginate.
-    public function index()
-    {
-        $posts = Post::orderByDesc('updated_at')->paginate(12);
+    public function index(Request $request)
+{
+    $searchTerm = $request->query('search'); //string de la barre de recherche
 
-        return view('feed.index', [
-            'posts' => $posts,
-        ]);
+    // Rechercher les utilisateurs correspondant au terme 'search' dans `username`
+    $users = User::query()
+        ->when($searchTerm, function ($query) use ($searchTerm) //quand on tape quelque chose dans la barre on entre dans la condition
+        {
+            $query->where('username', 'LIKE', '%' . $searchTerm . '%'); //là où username ressemble au string dans la barre de recherche
+        })
+        ->get(); //ca fait select dans la DB
 
 
-    }
+    // Récupérer les posts associés aux utilisateurs trouvés ou contenant le mot-clé dans `body`
+    $posts = Post::query()
+        ->when($searchTerm, function ($query) use ($users, $searchTerm) //quand on tape quelque chose dans la barre on entre dans la condition
+        {
+            $query->where(function ($query) use ($users, $searchTerm) //Cela définit une sous-requête qui va encapsuler plusieurs conditions.
+            {
+                $query->whereIn('user_id', $users->pluck('id')) //Where user_id in users filtrés
+                      ->orWhere('body', 'LIKE', '%' . $searchTerm . '%'); //or where body like string dans la barre de recherche
+            });
+        })
+        ->orderByDesc('published_at')
+        ->paginate(12)
+        ->withQueryString(); //conserve la recherche à travers la pagination
+
+    return view('feed.index', [
+        'users' => $users,
+        'posts' => $posts,
+    ]);
+}
+
 }
--- resources/views/feed/index.blade.php
+++ resources/views/feed/index.blade.php
@@ -1,15 +1,33 @@
 <x-guest-layout>
-  <h1 class="text-center font-bold text-2xl mb-8 text-gray-800">Liste des posts</h1>
+    <h1 class="font-bold text-xl mb-4">Liste des posts</h1>
 
-  <ul class="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 mx-auto max-w-screen-lg px-4">
-    @foreach ($posts as $post)
-      <li class="bg-white rounded-lg shadow-lg overflow-hidden">
+    <form action="{{ route('feed') }}" method="GET" class="mb-4">
+      <div class="flex items-center">
+        <input
+          type="text"
+          name="search"
+          id="search"
+          placeholder="Rechercher un post"
+          class="flex-grow border border-gray-300 rounded shadow px-4 py-2 mr-4"
+          value="{{ request()->search }}"
+          autofocus
+        />
+        <button
+          type="submit"
+          class="bg-white text-gray-600 px-4 py-2 rounded-lg shadow"
+        >
+          <x-heroicon-o-magnifying-glass class="h-5 w-5" />
+        </button>
+      </div>
+    </form>
+
+    <ul class="grid sm:grid-cols-2 lg:grid-cols-3 2xl:grid-cols-4 gap-8">
+      @foreach ($posts as $post)
+      <li>
         <x-post-card :post="$post" />
       </li>
-    @endforeach
-  </ul>
+      @endforeach
+    </ul>
 
-  <div class="mt-10 flex justify-center">
-    {{ $posts->links('pagination::tailwind') }}
-  </div>
-</x-guest-layout>
+    <div class="mt-8">{{ $posts->links() }}</div>
+  </x-guest-layout>
